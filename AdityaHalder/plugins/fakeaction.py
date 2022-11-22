13e2a0182f988b9338a53d64f6a2a6d90e895aad
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-11-22 17:26:03 +0700

    Update fakeaction.py

--- AdityaHalder/plugins/fakeaction.py
+++ AdityaHalder/plugins/fakeaction.py
@@ -1,4 +1,12 @@
-import asyncio
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/Lunatic0de
+
 from asyncio import sleep
 from pyrogram.raw import functions
 from pyrogram import *
